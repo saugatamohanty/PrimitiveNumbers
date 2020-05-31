31143c1dc5d4a3bd0ca9110ef69454263ee82596
saugatamohanty <saugatamohanty@gmail.com>
2020-05-31 18:16:45 -0400

    Added code changes to Main.java

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -3,7 +3,9 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
-
+        double value = .012;
+        double pSum = value+value+value;
+        System.out.println("Sum of primitives: " + pSum);
     }
 
 }
